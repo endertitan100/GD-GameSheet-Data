2298b83bd4280c41c931069336543628673f65aa
endertitan100 <broenhill.oliver@gmail.com>
2022-07-04 18:06:11 +0100

    Convertor Finished

--- Converter.py
+++ Converter.py
@@ -1,28 +1,58 @@
-
+import json
+import os
 
 def ConvFile(File):
-    f = open(File)
-    fData = f.read()
-    f.close()
+        f = open(File)
+        fData = f.read()
+        f.close()
 
-    u_Titles = fData.split("""</key>
+        u_Titles = fData.split("""</key>
             <dict>""")
 
-    u_Pos = fData.split("""</key>
+        u_Pos = fData.split("""</key>
                 <string>{{""")
-    u_Pos.pop(0)
 
-    for i in range(len(u_Titles)-1):
-        #Get the Title
-        #print(u_Titles[i])
-        t_End = u_Titles[i][::-1].find(">")
-        title = u_Titles[i][::-1][0:t_End][::-1]
-        #Get the Position
-        p_End = u_Pos[i].find("}")
-        pos = u_Pos[i][0:p_End]
-        #TODO GET SIZE AND OFFSET
+        u_Size = fData.split("""ceSize</key>
+                <string>{""")
 
+        u_Offset = fData.split("""ffset</key>
+                <string>{""")
+        u_Pos.pop(0)
+        u_Size.pop(0)
+        u_Offset.pop(0)
 
-        print(title,pos)
+        Converted = {}
 
-ConvFile("SD/DungeonSheet.plist")
+        for i in range(len(u_Titles)-1):
+                #Get Image Name
+                t_End = u_Titles[i][::-1].find(">")
+                title = u_Titles[i][::-1][0:t_End][::-1]
+                #Get the Position
+                p_End = u_Pos[i].find("}")
+                pos = u_Pos[i][0:p_End]
+                #Get the Size
+                s_End = u_Size[i].find("}")
+                size = u_Size[i][0:s_End]
+                #Get the Offset
+                o_End = u_Offset[i].find("}")
+                offset = u_Offset[i][0:o_End]
+                
+                FName = File[0:len(File)-6]
+                Converted[FName+"/"+title] = {"Positon":{},"Size":{},"Offset":{}}
+                Converted[FName+"/"+title]["Positon"] = pos.split(",")
+                Converted[FName+"/"+title]["Size"] = size.split(",")
+                Converted[FName+"/"+title]["Offset"] = offset.split(",")
+                
+        return Converted
+
+Dirs = {"SD","HD","UHD"}
+
+for Dir in Dirs: 
+        Final = {}
+        for File in os.listdir(Dir):
+                Final = Final | ConvFile(Dir+"/"+File)
+                print(File +" Converted Successfully")
+
+        f = open(Dir+"_GameSheet.json","w")
+        f.write(json.dumps(Final,sort_keys=True, indent=4))
+        f.close()
